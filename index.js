bee396fb75ed0c9e930a93a9aca03e4b39e79115
Minal Kharche <minal.berkeley@gmail.com>
2019-06-22 17:09:56 -0700

    added main functionality for game

--- index.js
+++ index.js
@@ -1,23 +1,72 @@
 var Word = require("./Word");
 var inquirer = require("inquirer");
 
-var cities = ["san francisco","fremont","berkeley"];
+var game = function () {
+    var cities = ["san francisco", "fremont", "berkeley"];
+    totalGuesses = 10;
+    var wordForGuess = new Word(cities[Math.floor(Math.random() * cities.length)]);
+    var sofarGuessedLetter = []
+    console.log(wordForGuess.getWord());
+    guessingStart();
+}
 
-var wordForGuess = new Word(cities[Math.floor(Math.random()*cities.length)]);
+var guessingStart = function () {
+    if (totalGuesses < 0) {
+        inquirer.prompt([{
+            type: "input",
+            message: "Guess a letter",
+            name: "letterGuessed",
+            validate: function (value) {
+                if (value.toLowerCase() !== value.toUpperCase()) {
+                    return true;
+                }
+                return false;
+            }
+        }]).then(function (response) {
+            if (sofarGuessedLetter.indexOf(response.letterGuessed) === -1) {
+                sofarGuessedLetter.push(response.letterGuessed);
+                var beforeGuessWord = wordForGuess.getWord();
+                wordForGuess.guessWord(response.letterGuessed);
+                console.log(wordForGuess.getWord());
+                var afterGuessWord = wordForGuess.getWord();
+                if (beforeGuessWord === afterGuessWord) {
+                    console.log("Wrong guess");
+                    totalGuesses--;
+                    if (totalGuesses === 0) {
+                        console.log("No more chance for guess")
+                    }
+                    else
+                        console.log("Guesses left: " + totalGuesses);
+                }
+                if (wordForGuess.getWord().indexOf("_") !== -1)
+                    guessingStart();
+                else {
+                    console.log("You guessed right word");
+                    askPlayAgain();
+                }
+            }
+            else {
+                console.log("Already guessed letter")
+                guessingStart();
+            }
+        });
+    }
+    else {
+    askToPlayAgain();
+    }
+}
+game();
 
-console.log(wordForGuess.getWord());
-
-inquirer.prompt([{
-    message:"Guess a letter",
-    name:"letterGuessed",
-    type:"input",
-    validate: function(value) {
-        if(value.toLowerCase()!== value.toUpperCase()) {
-          return true;
-        }
-        return false;
-      }
-    }]).then(function(response){
+function askToPlayAgain() {
+    inquirer.prompt([{
+        type: "confirm",
+        name: "playAgain",
+        message: "Play more?",
+        default: false
+    }]).then(function (response) {
         wordForGuess.guessWord(response.letterGuessed);
         console.log(wordForGuess.getWord());
-    }) 
+    });
+    if (response.playAgain)
+    game();
+}
