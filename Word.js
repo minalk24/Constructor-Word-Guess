445873df41b28b9308cdb85908130b21a01ad990
Minal Kharche <minal.berkeley@gmail.com>
2019-06-24 23:14:31 -0700

    updated with the functions logic in word.js

--- Word.js
+++ Word.js
@@ -1,26 +1,37 @@
 var Letter = require("./Letter");
 
-var Word = function(strWord){
-    this.getLetters = function(){
-        var letterArray=[];
-        for(var i =0;i<strWord.length;i++){
+var Word = function (strWord) {
+    this.getLetters = function () {
+        var letterArray = [];
+        for (var i = 0; i < strWord.length; i++) {
             letterArray.push(new Letter(strWord[i]));
         }
         return letterArray;
     };
+
     this.letters = this.getLetters();
 
-    this.getWord = function(){
+    this.getWord = function () {
         var strWord = "";
-        for(var i =0;i<this.letters.length;i++){
-            strWord+=this.letters[i].toString();
+        for (var i = 0; i < this.letters.length; i++) {
+            strWord += this.letters[i].getLetter();
         }
         return strWord;
     };
-    this.guessWord = function(char){
-        for(var i =0;i<this.letters.length;i++)
+    this.guessWord = function (char) {
+        for (var i = 0; i < this.letters.length; i++)
             this.letters[i].guessLetter(char);
+
+    };
+
+    this.displayWord = function () {
+        var strWord = "";
+        for (var i = 0; i < this.letters.length; i++) {
+            strWord += this.letters[i].displayLetter();
+        }
+        return strWord;
     };
 };
 
-module.exports = Word; 
+//export module
+module.exports = Word;
